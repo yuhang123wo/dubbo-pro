2e9acf61b7eba7d70b1c7967bdb7b5535a82b1b5
yuhang <yuhang@stargoto.com>
2018-11-28 15:43:00 +0800

    atomiks分布式事务

--- dubbo-test/src/main/java/cn/yh/st/service/impl/MuserServiceImpl.java
+++ dubbo-test/src/main/java/cn/yh/st/service/impl/MuserServiceImpl.java
@@ -4,17 +4,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import cn.yh.st.dao.one.MuserDao;
+import cn.yh.st.dao.two.UserDao;
 import cn.yh.st.domain.MUser;
+import cn.yh.st.domain.User;
 import cn.yh.st.service.MuserService;
 
 @Service
 public class MuserServiceImpl implements MuserService {
 	@Autowired
 	private MuserDao muserDao;
+	@Autowired
+	private UserDao userDao;
 
 	@Override
 	public int addMuser(MUser muser) {
+		User user = new User();
+		user.setName("gg");
+		userDao.insert(user);
+		int i=1/0;
 		return muserDao.insertSelective(muser);
 	}
-
 }
